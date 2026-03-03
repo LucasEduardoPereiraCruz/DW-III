81aef7a651a543d98c17601d02a55ea126afbf21
Lucas Eduardo Pereira da Cruz <lucas.cruz54@fatec.sp.gov.br>
2026-03-03 12:49:30 -0300

    Sistema Academico e removendo arquivos temporários e ignorando .vs

--- projetos/SistemaAcademico/SistemaAcademico/obj/Debug/net9.0/SistemaAcademico.AssemblyInfo.cs
+++ projetos/SistemaAcademico/SistemaAcademico/obj/Debug/net9.0/SistemaAcademico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaAcademico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33066e4f61842eacf5affd27423b6fdc8d43f811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f1d3d5cf57cfc7d164032715bbe414bb6ba419")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaAcademico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaAcademico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
